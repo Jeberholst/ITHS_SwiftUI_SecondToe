6af144d473972715b72b80988815833625f458f6
jMac <j_eberholst@gmail.com>
2021-01-24 16:22:59 +0100

    Initial Commit

---
+++ odot/odotApp.swift
@@ -0,0 +1,17 @@
+//
+//  odotApp.swift
+//  odot
+//
+//  Created by Joakim Eberholst on 2021-01-24.
+//
+
+import SwiftUI
+
+@main
+struct odotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
